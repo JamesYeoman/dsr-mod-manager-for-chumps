73eed5865bfb4e280f2e41dbf8c01e445536001a
James Yeoman <jamesyeoman10@gmail.com>
2022-01-13 12:47:37 +0000

    Change deployment to use gh-pages pattern

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
-import { defineConfig } from 'vite';
-import { genBuildOpts, maybeCloseStdin, plugins, root } from './vite/vite.common';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { ConfigEnv, defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => {
+  const devMode = configEnv.mode === 'development';
   maybeCloseStdin(configEnv);
-  return { root, plugins, build: genBuildOpts(configEnv, 'dist') };
+  return {
+    root: resolve(__dirname, 'frontend'),
+    plugins: [react()],
+    build: {
+      // Move dist out of frontend folder
+      outDir: resolve(__dirname, 'dist'),
+      emptyOutDir: !devMode,
+      sourcemap: devMode,
+      minify: devMode,
+    },
+  };
 });
+
+// https://github.com/vitejs/vite/issues/5743
+function maybeCloseStdin(configEnv: ConfigEnv) {
+  if (configEnv.command === 'build') return;
+  process.stdin.on('close', () => {
+    process.exit(0);
+  });
+  process.stdin.resume();
+}
