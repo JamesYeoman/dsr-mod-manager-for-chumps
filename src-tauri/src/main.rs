a4267e1998d1a19e009cdc789ba4fd43a47ffcea
James Yeoman <jamesyeoman10@gmail.com>
2022-01-20 16:18:59 +0000

    Update the tauri backend dummy data structure

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,23 +4,51 @@
 )]
 
 #[derive(serde::Serialize)]
+
 struct UiModListData {
-  content: String,
-  selected: bool,
+  id: &'static str,
+  content: &'static str,
 }
 
 #[tauri::command]
 fn get_mod_list() -> Result<Vec<UiModListData>, String> {
   let result: Vec<UiModListData> = vec![
-    UiModListData { content: "This is an item".into(), selected: false},
-    UiModListData { content: "This is another item".into(), selected: false},
-    UiModListData { content: "This is a third item".into(), selected: false},
-    UiModListData { content: "This is a fourth item".into(), selected: false},
-    UiModListData { content: "This is a fifth item".into(), selected: false},
-    UiModListData { content: "This is a sixth item".into(), selected: true},
-    UiModListData { content: "This is a seventh item".into(), selected: false},
-    UiModListData { content: "This is an eighth item".into(), selected: false},
-    UiModListData { content: "This is a ninth item".into(), selected: false}
+    UiModListData {
+      id: "mod-item-1",
+      content: "This is an item",
+    },
+    UiModListData {
+      id: "mod-item-2",
+      content: "This is another item",
+    },
+    UiModListData {
+      id: "mod-item-3",
+      content: "This is a third item",
+    },
+    UiModListData {
+      id: "mod-item-4",
+      content: "This is a fourth item",
+    },
+    UiModListData {
+      id: "mod-item-5",
+      content: "This is a fifth item",
+    },
+    UiModListData {
+      id: "mod-item-6",
+      content: "This is a sixth item",
+    },
+    UiModListData {
+      id: "mod-item-7",
+      content: "This is a seventh item",
+    },
+    UiModListData {
+      id: "mod-item-8",
+      content: "This is an eighth item",
+    },
+    UiModListData {
+      id: "mod-item-9",
+      content: "This is a ninth item",
+    },
   ];
 
   Ok(result)
@@ -28,7 +56,7 @@ fn get_mod_list() -> Result<Vec<UiModListData>, String> {
 
 fn main() {
   tauri::Builder::default()
-  .invoke_handler(tauri::generate_handler![get_mod_list])
+    .invoke_handler(tauri::generate_handler![get_mod_list])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
